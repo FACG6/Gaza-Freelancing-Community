28dd38aab78d10be4870c9cc5e4398d0c970f9f5
AhmedAl-Almi <ahmedalmi95@gmail.com>
2019-03-19 16:09:23 +0200

    edit pull request and remove eslint from this pull requist #34

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 
+const cookieParser = require('cookie-parser');
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false,
+}));
+
+app.use(cookieParser());
 
 app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req,res) => {
+  res.send('hello from server')
+})
 
 module.exports = app;
