78ba3cb12db7709254e8985651f354d1a7dadef3
AhmedAl-Almi <ahmedalmi95@gmail.com>
2019-03-20 16:15:31 +0200

    test query add user

---
+++ test/test.js
@@ -0,0 +1,80 @@
+const tap = require('tape');
+
+const reBuild = require('../src/database/config/db_build');
+const { addUser } = require('../src/database/queries/addData');
+
+tap('test add user for firstname', (t) => {
+  reBuild()
+    .then(addUser({
+      firstname: 'Ahmed',
+      lastname: 'Alami',
+      mobile_number: 12345454,
+      email: 'ahmed@gmail.com',
+      freelancer_url: 'ww.ass.com',
+      photo_url: 'www.hhhh.cs',
+      birthday: null,
+      city: 'gaza',
+      password: '123',
+    }).then((res) => {
+      t.equal(res.rows[0].firstname, 'Ahmed', 'must be Ahmed');
+      t.end();
+    }).catch(err => t.error(err)))
+    .catch(errr => t.error(errr));
+});
+
+tap('test add user for lastname', (t) => {
+  reBuild()
+    .then(addUser({
+      firstname: 'Ahmed',
+      lastname: 'Elalmi',
+      mobile_number: 12345454,
+      email: 'ahmed@gmail.com',
+      freelancer_url: 'ww.ass.com',
+      photo_url: 'www.hhhh.cs',
+      birthday: null,
+      city: 'gaza',
+      password: '123',
+    }).then((res) => {
+      t.equal(res.rows[0].lastname, 'Elalmi', 'must be Elalmi');
+      t.end();
+    }).catch(err => t.error(err)))
+    .catch(errr => t.error(errr));
+});
+
+tap('test add user for lastname', (t) => {
+  reBuild()
+    .then(addUser({
+      firstname: 'Ahmed',
+      lastname: 'Elalmi',
+      mobile_number: 12345454,
+      email: 'ahmed@gmail.com',
+      freelancer_url: 'ww.ass.com',
+      photo_url: 'www.hhhh.cs',
+      birthday: null,
+      city: 'gaza',
+      password: '123',
+    }).then((res) => {
+      t.equal(res.rows[0].lastname, 'Elalmi', 'must be Elalmi');
+      t.end();
+    }).catch(err => t.error(err)))
+    .catch(errr => t.error(errr));
+});
+
+tap('test add user for mobile number', (t) => {
+  reBuild()
+    .then(addUser({
+      firstname: 'Ahmed',
+      lastname: 'Elalmi',
+      mobile_number: 12345,
+      email: 'ahmed@gmail.com',
+      freelancer_url: 'ww.ass.com',
+      photo_url: 'www.hhhh.cs',
+      birthday: null,
+      city: 'gaza',
+      password: '123',
+    }).then((res) => {
+      t.equal(res.rows[0].mobile_number, 12345, 'the mobile_number must be 12345');
+      t.end();
+    }).catch(err => t.error(err)))
+    .catch(errr => t.error(errr));
+});
