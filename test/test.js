959d4b394c280d4e14c5f00c4420199d9b353281
AhmedAl-Almi <ahmedalmi95@gmail.com>
2019-03-20 21:51:19 +0200

    add new test for city

--- test/test.js
+++ test/test.js
@@ -41,25 +41,6 @@ tap('test add user for lastname', (t) => {
     .catch(errr => t.error(errr));
 });
 
-tap('test add user for lastname', (t) => {
-  reBuild()
-    .then(addUser({
-      firstname: 'Ahmed',
-      lastname: 'Elalmi',
-      mobile_number: 12345454,
-      email: 'ahmed@gmail.com',
-      freelancer_url: 'ww.ass.com',
-      photo_url: 'www.hhhh.cs',
-      birthday: null,
-      city: 'gaza',
-      password: '123',
-    }).then((res) => {
-      t.equal(res.rows[0].lastname, 'Elalmi', 'must be Elalmi');
-      t.end();
-    }).catch(err => t.error(err)))
-    .catch(errr => t.error(errr));
-});
-
 tap('test add user for mobile number', (t) => {
   reBuild()
     .then(addUser({
@@ -74,6 +55,25 @@ tap('test add user for mobile number', (t) => {
       password: '123',
     }).then((res) => {
       t.equal(res.rows[0].mobile_number, 12345, 'the mobile_number must be 12345');
+      t.end();
+    }).catch(err => t.error(err)))
+    .catch(errr => t.error(errr));
+});
+
+tap('test add user for city', (t) => {
+  reBuild()
+    .then(addUser({
+      firstname: 'Ahmed',
+      lastname: 'Elalmi',
+      mobile_number: 12345454,
+      email: 'ahmed@gmail.com',
+      freelancer_url: 'ww.ass.com',
+      photo_url: 'www.hhhh.cs',
+      birthday: null,
+      city: 'gaza',
+      password: '123',
+    }).then((res) => {
+      t.equal(res.rows[0].city, 'gaza', 'must be gaza');
       t.end();
     }).catch(err => t.error(err)))
     .catch(errr => t.error(errr));
