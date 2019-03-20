d7d4af849fbc93d797a3670fd78f4fa5c71f30c5
Angham116 <angham.1161995@hotmail.com>
2019-03-20 16:07:57 +0200

    rendered login page #15

---
+++ src/controllers/login.js
@@ -0,0 +1,8 @@
+exports.get = (request, response) => {
+  response.render('login', {
+    js: ['collectData', 'login'],
+    css: ['login'],
+    layout: 'login_signup',
+    title: 'Login | Gaza Freelancing Community',
+  });
+};
