a3f7b72eeb3b0de7da83d6efa824915266fe4f3a
Angham116 <angham.1161995@hotmail.com>
2019-03-21 12:10:56 +0200

    changed password field name from pass to password

--- src/database/queries/getData.js
+++ src/database/queries/getData.js
@@ -1,7 +1,7 @@
 const connect = require('./../config/db_connection');
 
 const checkPassword = (pass) => {
-  const sql = 'select password from users where pass = $1';
+  const sql = 'select password from users where password = $1';
   const value = [pass];
   return connect.query(sql, value);
 };
