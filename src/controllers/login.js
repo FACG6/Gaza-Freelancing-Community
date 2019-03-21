e1a4c64db93b837eaca4aee42229e02ba90f706d
Angham116 <angham.1161995@hotmail.com>
2019-03-21 15:03:46 +0200

    deleted un needed codes

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,18 +1,8 @@
 exports.get = (request, response) => {
   response.render('login', {
-    js: ['collectData', 'login'],
+    js: ['helpers/collectData', 'login'],
     css: ['login'],
     layout: 'login_signup',
     title: 'Login | Gaza Freelancing Community',
   });
-};
-
-exports.post = (request, response) => {
-  console.log('jjjjj');
-  
-  // console.log(request.body);
-  const {email, password} = request.body;
-  console.log(email);
-  
-
 };
