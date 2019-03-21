8a0f743dfccc07e30f5068d1ec69fd6c6c4cb29a
Fatma Siam <fatma.o.siam@gmail.com>
2019-03-21 10:55:05 +0200

    type of number and insert into data

--- src/database/config/db_build.sql
+++ src/database/config/db_build.sql
@@ -21,7 +21,7 @@ CREATE TABLE users (
   freelancer_url varchar NOT NULL,
 	photo_url VARCHAR NOT NULL,
 	birthday TIMESTAMP ,
-	mobile_number INT NOT NULL UNIQUE,
+	mobile_number TEXT NOT NULL UNIQUE,
 	specalization_id INT REFERENCES specialization(id),
   city VARCHAR(20),
 	password VARCHAR NOT NULL
@@ -41,5 +41,20 @@ CREATE TABLE requirement (
 	text VARCHAR NOT NULL ,
 	prop_id INT REFERENCES proposal(id)
 );
+
+INSERT INTO field (name) 
+values ('Developer');
+
+INSERT INTO specialization (name,field_id) 
+values ('web develpoer',1);
+
+INSERT INTO users (firstname,lastname,email,freelancer_url,photo_url,mobile_number,specalization_id,password)
+values('fatma','siam','f.siam@gmail.com','https://mm.mm.mmmmm','https://mm.mm.mmmmm','05333',1,'123123');
+
+INSERT INTO proposal (title,description,user_id,specalization_id,contact_me)
+values ('front-end develpoer','we need a front-end developer to working at project',1,1,'f.siam@gmail.com');
+
+INSERT INTO requirement(text,prop_id) 
+values ('full time' ,1), ('react',1);
 
 COMMIT ;
