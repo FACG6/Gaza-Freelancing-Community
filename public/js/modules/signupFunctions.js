6d562beda55ec12089719d9c2c859fb984bc6a03
Anies <a2nies2012@gmail.com>
2019-03-21 09:38:20 +0200

    SignUpDom

--- public/js/modules/signupFunctions.js
+++ public/js/modules/signupFunctions.js
@@ -1,22 +1,19 @@
-const validate = (field, regex, cb => {
+const validate = (field, regex, cb) => {
     let notAccepteable = [];
     const keys = Object.keys(field)
     for (let i = 0; i < keys.length; i++) {
-        if (!field[keys[i]].match(regex)) {
-            unaccepteable.push(field[key]);
+        if (!regex[i].test(field[keys[i]])) {
+            notAccepteable.push(keys[i]);
         }
     }
     if (notAccepteable.length > 0) {
-         swal(text: `make sure you fill the following fields ${unaccepteable.join(',')}`, type: "warning");
-         return cb(false);
+        swal(text: `make sure you fill the following fields ${unaccepteable.join(',')}`, type: "warning");
+        return cb(false);
     }
     return cb(true);
-});
-
+};
 const ToggleDisplay = (prev, next) => {
     prev.style.display = "none";
     next.style.display = "block";
 };
-
-
 module.exports = { validate, ToggleDisplay };
--- public/js/signup.js
+++ public/js/signup.js
@@ -17,21 +17,72 @@ const password = document.getElementById('password');
 const confirmedPassword = document.getElementById('confirmedPassword');
 const signUp = document.getElementById('signUp');
 const { validate, ToggleDisplay } = require('./modules/signupFunctions');
-
 const signupData = {};
+const Error = '';
+
 firstNext.addEventListener('click', () => {
-    firstStepfields = {
+    const firstStepfields = {
         firstName: firstName.value,
         lastName: lastName.value,
         mobile: mobile.value,
         email: email.value,
     };
-    const firstStepValidationRegex = ['/[a-z]{3,10}/g', '/[a-z]{3,10}/g', '/^(\+\d{1,3}[- ]?)?\d{9}$/', '/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'];
-
+    const firstStepValidationRegex = [/[a-z]{3,10}/g, /[a-z]{3,10}/g, /^(\+\d{1,3}[- ]?)?\d{9}$/, /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/];
     validate(firstStepfields, firstStepValidationRegex, (trueOrFalse) => {
         if (trueOrFalse) {
             ToggleDisplay(firstStepDiv, secondStepDiv);
-            signupData = Object.assign(signupData,firstStepfields);
+            signupData = Object.assign(signupData, firstStepfields);
         }
+    });
+});
+secondNext.addEventListener('click', () => {
+    const secondStepfields = {
+        field: field.value,
+        specialization: specialization.value,
+        freeLanceURL: freeLanceURL.value,
+        photoURL: photoURL.value,
+    };
+    const secondStepValidationRegex = [/^[a-zA-Z\s\D+]/g, /^[a-zA-Z\s\D+]/g, /http(s)?:\/\/([\w]+\.)?[A-z0-9_-]+\.com\/[A-z0-9_-]+/, /^https?:\/\/([\w]||[-_.])+\.[a-z]{2,4}(\/||[\w-_])*\.(png|jpe?g||gif)$/];
+    validate(secondStepfields, secondStepValidationRegex, (trueOrFalse) => {
+        if (trueOrFalse) {
+            ToggleDisplay(secondStepDiv, thirdStepDiv);
+            signupData = Object.assign(signupData, secondStepfields);
+        }
+    });
+});
+fisrstBack.addEventListener('click', () => {
+    ToggleDisplay(secondStepDiv, firstStepDiv);
+});
+signUp.addEventListener('click', () => {
+    const thirdStepfields = {
+        password: password.value
+    };
+    const thirdStepValidationRegex = [/^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{8,})/g];
+    validate(secondStepfields, secondStepValidationRegex, (trueOrFalse) => {
+        if (trueOrFalse) {
+            signupData = Object.assign(signupData, secondStepfields);
+            fetch('/signup', {
+                method: 'POST',
+                credentials: 'same-origin',
+                body: JSON.stringify(signupData),
+                headers: { 'Content-Type': 'application/json' },
+            })
+                .then(res => res.json())
+                .then((res) => {
+                    if (res.error) {
+                        error.textContent = res.error;
+                    } else {
+                        window.location.href = '/login';
+                    }
+                })
+                .catch(e => {
+                    swal(text: 'Error, please try again..', type: "warning");
+                });
+        }
+    });
+});
+secondBack.addEventListener(()=>{
+    fisrstBack.addEventListener('click', () => {
+        ToggleDisplay(thirdStepDiv,secondStepDiv);
     });
 });
