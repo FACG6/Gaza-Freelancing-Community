039a59b7b04920c3c0ca18d15f5f3fe73cb8c8ab
Angham116 <angham.1161995@hotmail.com>
2019-03-21 08:08:31 +0200

    made query to check user email from database

---
+++ src/database/queries/getData.js
@@ -0,0 +1,9 @@
+const connect = require('./../config/db_connection');
+
+const checkEmail = (email) => {
+  const sql = 'SELECT email from users where email = $1';
+  const value = [email];
+  return connect.query(sql, value);
+};
+
+module.exports = checkEmail;
