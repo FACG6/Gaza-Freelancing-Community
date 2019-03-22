86533887000ed5b8c8c97c3a6839d6736b10e41f
Angham116 <angham.1161995@hotmail.com>
2019-03-22 15:19:00 +0200

    edited

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,11 +1,10 @@
 const pageTitle = require('./../views/helpers/makeTitle');
-console.log('iiiiiiii');
 
 exports.get = (request, response) => {
   response.render('login', {
     js: ['helpers/collectData', 'login'],
     css: ['login'],
     layout: 'login_signup',
-    title: pageTitle('Login'),
+    title: pageTitle.pageTitle('Login'),
   });
 };
