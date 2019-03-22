cb88e44a41bcdd06474d56f0ad5baebb2c9a8225
AhmedAl-Almi <ahmedalmi95@gmail.com>
2019-03-22 20:23:14 +0200

    Shortcut forr function

--- src/database/queries/addData.js
+++ src/database/queries/addData.js
@@ -1,20 +1,10 @@
 const connection = require('../config/db_connection');
 
 const addUser = (userInfo) => {
+  const queryValues = Object.keys(userInfo).map(info => userInfo[info]);
   const queryObj = {
     text: 'insert into users (firstname, lastname, mobile_number, email, freelancer_url, photo_url,birthday, specalization_id,city,password ) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *',
-    values: [
-      userInfo.firstname,
-      userInfo.lastname,
-      userInfo.mobile_number,
-      userInfo.email,
-      userInfo.freelancer_url,
-      userInfo.photo_url,
-      userInfo.birthday,
-      userInfo.specalization_id,
-      userInfo.city,
-      userInfo.password,
-    ],
+    values: [...queryValues],
   };
   return connection.query(queryObj);
 };
