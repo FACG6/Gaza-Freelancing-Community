bfd9a38d155c47349966fd533bee6690e882d23f
Fatma Siam <fatma.o.siam@gmail.com>
2019-03-23 16:56:55 +0300

    using the error route

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const error = require('./error');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('<h1 style="text-align:center; color:red; padding-top:100px">Hello from Gaza Freelancing Community<br><br>GFC</h1>');
-});
 
+router.use(error.notfound);
+router.use(error.serverError);
 module.exports = router;
