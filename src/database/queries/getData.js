cbce969f94ddd5ca2b6c0eca378691ac190d035c
Fatma Siam <fatma.o.siam@gmail.com>
2019-03-23 16:00:45 +0300

    making checkemail and mobile function

---
+++ src/database/queries/getData.js
@@ -0,0 +1,17 @@
+const connect = require('./../config/db_connection');
+
+const checkMobile = (mobile) => {
+  const query = {
+    text: 'select * from users where  mobile_number = $1',
+    values: [mobile],
+  };
+  return connect.query(query);
+};
+
+const checkEmail = (email) => {
+  const sql = 'SELECT * from users where email = $1';
+  const value = [email];
+  return connect.query(sql, value);
+};
+
+module.exports = { checkMobile, checkEmail };
