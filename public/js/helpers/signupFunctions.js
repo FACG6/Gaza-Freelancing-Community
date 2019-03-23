8aed95b305f5a9d8844b7c82a6c90cdae860fb2d
Anies <a2nies2012@gmail.com>
2019-03-23 16:06:00 +0200

    signup Functions

--- public/js/helpers/signupFunctions.js
+++ public/js/helpers/signupFunctions.js
@@ -1,29 +1,32 @@
 const validate = (field, regex, cb) => {
-    let notAccepteable = [];
-    const keys = Object.keys(field)
+    const notAccepteable = [];
+    const keys = Object.keys(field);
     for (let i = 0; i < keys.length; i++) {
-        if (!regex[i].test(field[keys[i]])) {
-            notAccepteable.push(keys[i]);
-        }
+      if (!regex[i].test(field[keys[i]])) {
+        notAccepteable.push(keys[i]);
+      }
     }
     if (notAccepteable.length > 0) {
-        swal({
-            text: `make sure you fill the following fields ${unaccepteable.join(',')}`,
-            type: "warning"
-        });
-        return cb(false);
+      swal({
+        text: `make sure you fill the following fields ${notAccepteable.join(',')}`,
+        type: 'warning',
+      });
+      return cb(false);
     }
     return cb(true);
-};
-const ToggleDisplay = (prev, next) => {
-    prev.style.display = "none";
-    next.style.display = "block";
-};
-const getElement = (val, id) => {
+  };
+  const ToggleDisplay = (prev, next) => {
+    prev.style.display = 'none';
+    next.style.display = 'block';
+  };
+  const getElement = (val, id) => {
     let item;
     if (val) {
-        return item = document.getElementById(id).value;
+      item = document.getElementById(id).value;
+      return item;
     }
-    return item = document.getElementById(id);
-}
-module.exports = { validate, ToggleDisplay, getElement, getBtn };
+    item = document.getElementById(id);
+    return item;
+  };
+  module.exports = { validate, ToggleDisplay, getElement };
+  
