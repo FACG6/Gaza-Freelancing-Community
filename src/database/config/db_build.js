90e320fd3b0fd1e650af75dbbfec82ecf431d67b
Angham116 <angham.1161995@hotmail.com>
2019-03-23 21:09:15 +0200

    restoration db_build.js file

---
+++ src/database/config/db_build.js
@@ -0,0 +1,12 @@
+
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const connection = require('./db_connection');
+
+const reBuildDB = () => {
+  const filepath = join(__dirname, 'db_build.sql');
+  const sql = readFileSync(filepath).toString();
+  return connection.query(sql);
+};
+
+module.exports = reBuildDB;
