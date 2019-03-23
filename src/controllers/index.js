193d1e2c49e340ee2eb024499b13ea2d7e4b32dd
Anies12 <a2nies2012@gmail.com>
2019-03-23 16:10:24 +0200

    Delete index.js

--- src/controllers/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const login = require('./login');
-
-router.get('/login', login.get);
-router.post('/login',login.post);
-
-module.exports = router;
