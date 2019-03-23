644b1eb7243d1a2ed5288f56e1d4ead81e0be7ef
Angham116 <angham.1161995@hotmail.com>
2019-03-23 16:10:06 +0200

    added title to login route

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,10 +1,8 @@
-const pageTitle = require('./../views/helpers/makeTitle');
-
 exports.get = (request, response) => {
   response.render('login', {
     js: ['helpers/collectData', 'login'],
     css: ['login'],
     layout: 'login_signup',
-    title: pageTitle.titleName('Login'),
+    title: 'Login',
   });
 };
