31b9c4488eb5ae7cea2b1603d7bd971d7311342c
Fatma Siam <fatma.o.siam@gmail.com>
2019-03-23 15:57:53 +0300

    making signup route

---
+++ src/controllers/signup.js
@@ -0,0 +1,57 @@
+const Joi = require('joi');
+const { addUser } = require('../database/queries/addData');
+const hashPassword = require('../helpers/hashPass');
+const { signUpSchema } = require('./../helpers/validation-shemas');
+const { checkMobile, checkEmail } = require('../database/queries/getData');
+
+exports.get = (req, res) => {
+  res.render('signup', {
+    layout: 'login_signup',
+    title: 'signup ',
+    js: ['collectData', 'signup'],
+    css: ['signup'],
+  });
+};
+
+exports.post = (req, res) => {
+  const user = req.body;
+  const userInfo = {
+    ...user.firstSection,
+    ...user.secondSection,
+    ...user.thirdSection,
+  };
+  const { error } = Joi.validate(userInfo, signUpSchema);
+  if (!error) {
+    checkEmail(userInfo.email)
+      .then((result) => {
+        if (result.rows[0]) {
+          res.status(400).send(JSON.stringify({ Error: ` This Email :  ${result.rows[0].email} is already register` }));
+        } else {
+          checkMobile(userInfo.mobile_number.trim())
+            .then((resultno) => {
+              if (resultno.rows[0]) {
+                res.status(400).send(JSON.stringify({ Error: ` This number :  ${resultno.rows[0].mobile_number} is already register` }));
+              } else {
+                hashPassword(userInfo.password)
+                  .then((hashedPass) => {
+                    userInfo.password = hashedPass;
+                    addUser(userInfo)
+                      .then(() => {
+                        res.status(201).send(JSON.stringify({ success: 'Regestration success ...', user: { ...user.firstSection } }));
+                      })
+                      .catch(() => {
+                        res.status(400).send(JSON.stringify({ Error: 'Bad Request ...' }));
+                      });
+                  });
+              }
+            }).catch(() => {
+              res.status(400).send(JSON.stringify({ Error: 'Bad Request' }));
+            });
+        }
+      }).catch(() => {
+        res.status(400).send(JSON.stringify({ Error: 'Bad Request' }));
+      });
+  } else {
+    res.status(400).send(JSON.stringify({ Error: 'Bad Request' }));
+  }
+};
