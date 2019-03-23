1f3653ac37d99961e107434b100fe7e2b24647e3
Angham116 <angham.1161995@hotmail.com>
2019-03-23 16:39:02 +0200

    Update db_build.sql

--- src/database/config/db_build.sql
+++ src/database/config/db_build.sql
@@ -45,4 +45,5 @@ CREATE TABLE requirement (
 INSERT INTO users(firstname, lastname, email, freelancer_url, photo_url, mobile_number, password)
 VALUES ('a', 'b', 'a.gmail.com', 'https://github.com/angham', 'https://www.iconspng.com/image/36709/face-avatar-man-male-handsome-3', '0097234567', '1234');
 
-COMMIT
+COMMIT;
+
