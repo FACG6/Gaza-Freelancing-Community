75ef65b883bec4299a5f999e77b0cabeabb2218f
Fatma Siam <fatma.o.siam@gmail.com>
2019-03-23 15:59:01 +0300

    using the route signup

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const signup = require('./signup');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('<h1 style="text-align:center; color:red; padding-top:100px">Hello from Gaza Freelancing Community<br><br>GFC</h1>');
 });
+
+router.get('/signup', signup.get);
+router.post('/signup', signup.post);
 
 module.exports = router;
