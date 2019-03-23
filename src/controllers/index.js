91284e97c95217ac77039ac4747a84e28705f7e2
Angham116 <angham.1161995@hotmail.com>
2019-03-23 10:52:55 +0200

    made logout router #30

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const logout = require('./logout');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('<h1 style="text-align:center; color:red; padding-top:100px">Hello from Gaza Freelancing Community<br><br>GFC</h1>');
 });
+
+router.get('/logout', logout);
 
 module.exports = router;
