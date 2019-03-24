755d15fb01cbb8e13c46cd883164a2216afc8396
AhmedAl-Almi <ahmedalmi95@gmail.com>
2019-03-24 10:45:05 +0300

    solve conflicts and eslint

--- test/test.js
+++ test/test.js
@@ -1,24 +1,7 @@
-// const supertest = require('supertest');
 const tape = require('tape');
-// const router = require('../src/app');
 
 const reBuildDB = require('../src/database/config/db_build');
 const { addUser } = require('../src/database/queries/addData');
-
-
-// test('Test Login route ', (t) => {
-//   supertest(router)
-//     .get('/login')
-//     .expect(200)
-//     .expect('content-type', /html/)
-//     .end((error, res) => {
-//       if (error) {
-//         t.error(error);
-//       }
-//       t.equal(typeof res.body, 'object', 'should return type of body object');
-//       t.end();
-//     });
-// });
 
 tape('test add user for firstname', (t) => {
   reBuildDB()
