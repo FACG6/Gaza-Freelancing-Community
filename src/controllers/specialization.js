ba18cd832039f262bfb6e25f407a141476c2933c
Fatma Siam <fatma.o.siam@gmail.com>
2019-03-24 14:36:30 +0300

    Edit speclization

--- src/controllers/specialization.js
+++ src/controllers/specialization.js
@@ -3,6 +3,6 @@ const { getSpecalize } = require('../database/queries/getData');
 module.exports = (req, res) => {
   const { categoryId } = req.body;
   getSpecalize(categoryId)
-  .then((special) =>  res.status(200).send({ special }))
+  .then(({rows:speclize}) =>  res.status(200).send({ speclize }))
   .catch(() => res.status(400).send({ Error: 'Bad Request ...' }));
 };
