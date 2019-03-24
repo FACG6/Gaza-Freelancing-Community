66106f971dba40b41afd6d349da964ef7591747c
AhmedAl-Almi <ahmedalmi95@gmail.com>
2019-03-24 10:38:50 +0300

    solved conflicts

--- test/test.js
+++ test/test.js
@@ -1,102 +1,26 @@
-const supertest = require('supertest');
-const test = require('tape');
-const router = require('../src/app');
+// const supertest = require('supertest');
+const tape = require('tape');
+// const router = require('../src/app');
 
 const reBuildDB = require('../src/database/config/db_build');
 const { addUser } = require('../src/database/queries/addData');
 
 
-test('test add user for firstname', (t) => {
-  reBuildDB()
-    .then(addUser({
-      firstname: 'Ahmed',
-      lastname: 'Alami',
-      mobile_number: 12345454,
-      email: 'ahmed@gmail.com',
-      freelancer_url: 'ww.ass.com',
-      photo_url: 'www.hhhh.cs',
-      birthday: null,
-      city: 'gaza',
-      password: '123',
-    }).then((res) => {
-      t.equal(res.rows[0].firstname, 'Ahmed', 'must be Ahmed');
-      t.end();
-    }).catch(err => t.error(err)))
-    .catch(errr => t.error(errr));
-});
+// test('Test Login route ', (t) => {
+//   supertest(router)
+//     .get('/login')
+//     .expect(200)
+//     .expect('content-type', /html/)
+//     .end((error, res) => {
+//       if (error) {
+//         t.error(error);
+//       }
+//       t.equal(typeof res.body, 'object', 'should return type of body object');
+//       t.end();
+//     });
+// });
 
-test('test add user for lastname', (t) => {
-  reBuildDB()
-    .then(addUser({
-      firstname: 'Ahmed',
-      lastname: 'Elalmi',
-      mobile_number: 12345454,
-      email: 'ahmed@gmail.com',
-      freelancer_url: 'ww.ass.com',
-      photo_url: 'www.hhhh.cs',
-      birthday: null,
-      city: 'gaza',
-      password: '123',
-    }).then((res) => {
-      t.equal(res.rows[0].lastname, 'Elalmi', 'must be Elalmi');
-      t.end();
-    }).catch(err => t.error(err)))
-    .catch(errr => t.error(errr));
-});
-
-test('test add user for mobile number', (t) => {
-  reBuildDB()
-    .then(addUser({
-      firstname: 'Ahmed',
-      lastname: 'Elalmi',
-      mobile_number: 12345,
-      email: 'ahmed@gmail.com',
-      freelancer_url: 'ww.ass.com',
-      photo_url: 'www.hhhh.cs',
-      birthday: null,
-      city: 'gaza',
-      password: '123',
-    }).then((res) => {
-      t.equal(res.rows[0].mobile_number, 12345, 'the mobile_number must be 12345');
-      t.end();
-    }).catch(err => t.error(err)))
-    .catch(errr => t.error(errr));
-});
-
-test('test add user for city', (t) => {
-  reBuildDB()
-    .then(addUser({
-      firstname: 'Ahmed',
-      lastname: 'Elalmi',
-      mobile_number: 12345454,
-      email: 'ahmed@gmail.com',
-      freelancer_url: 'ww.ass.com',
-      photo_url: 'www.hhhh.cs',
-      birthday: null,
-      city: 'gaza',
-      password: '123',
-    }).then((res) => {
-      t.equal(res.rows[0].city, 'gaza', 'must be gaza');
-      t.end();
-    }).catch(err => t.error(err)))
-    .catch(errr => t.error(errr));
-});
-
-test('Test Login route ', (t) => {
-  supertest(router)
-    .get('/login')
-    .expect(200)
-    .expect('content-type', /html/)
-    .end((error, res) => {
-      if (error) {
-        t.error(error);
-      }
-      t.equal(typeof res.body, 'object', 'should return type of body object');
-      t.end();
-    });
-});
-
-test('test add user for firstname', (t) => {
+tape('test add user for firstname', (t) => {
   reBuildDB()
     .then(() => addUser({
       firstname: 'Ahmed',
@@ -117,7 +41,7 @@ test('test add user for firstname', (t) => {
     });
 });
 
-test('test add user for lastname', (t) => {
+tape('test add user for lastname', (t) => {
   reBuildDB()
     .then(() => addUser({
       firstname: 'Ahmed',
@@ -139,7 +63,7 @@ test('test add user for lastname', (t) => {
     });
 });
 
-test('test add user for mobile number', (t) => {
+tape('test add user for mobile number', (t) => {
   reBuildDB()
     .then(() => addUser({
       firstname: 'Ahmed',
