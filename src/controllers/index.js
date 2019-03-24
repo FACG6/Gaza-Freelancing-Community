22747faef76abf4a0c6e25901307b5f00af58a27
Fatma Siam <fatma.o.siam@gmail.com>
2019-03-24 12:48:19 +0300

    remove unnesesary lines

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,9 +3,6 @@ const signup = require('./signup');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('<h1 style="text-align:center; color:red; padding-top:100px">Hello from Gaza Freelancing Community<br><br>GFC</h1>');
-});
 
 router.get('/signup', signup.get);
 router.post('/signup', signup.post);
