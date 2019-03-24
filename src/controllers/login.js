f2be5c6b2397364a6a03a86fa0bbcea34137b83a
Anies <a2nies2012@gmail.com>
2019-03-24 21:20:46 +0200

    login Post

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const getData = require('../../src/database/queries/getData');
 const pageTitle = require('../views/helpers/makeTitle');
+const { validationRegex } = require('../../public/js/helpers/signup');
 
 exports.get = (request, response) => {
   response.render('login', {
@@ -11,20 +12,25 @@ exports.get = (request, response) => {
   });
 };
 exports.post = (request, response, next) => {
-  getData.checkEmail(request.body.email.trim())
-    .then((res) => {
-      if (res.rows) {
-        return bcrypt.compare(request.body.password, res.rows[0].password);
-      }
-      response.send({ ErrMsg: 'No such email!' });
-    }).then((result) => {
-      if (result) {
-        next();
-      } else {
-        response.send({ ErrMsg: 'Wrong password' });
-      }
-    })
-    .catch(() => {
-      response.status(500).send({ ErrMsg: 'Internal server error' });
-    });
+  if (validationRegex.firstStepValidationRegex[3].test(request.body.email.trim())) {
+    getData.checkEmail(request.body.email.trim())
+      .then((res) => {
+        if (res.rows) {
+          if (validationRegex.thirdStepValidationRegex[0].test(request.body.password)) {
+            return bcrypt.compare(request.body.password, res.rows[0].password);
+          }
+          response.send({ ErrMsg: 'Wrong Password' });
+        }
+        response.send({ ErrMsg: 'No such email!' });
+      }).then((result) => {
+        if (result) {
+          next();
+        } else {
+          response.send({ ErrMsg: 'Wrong password' });
+        }
+      })
+      .catch(() => {
+        response.status(500).send({ ErrMsg: 'Internal server error' });
+      });
+  }
 };
--- src/controllers/middlewares/createCookie.js
+++ src/controllers/middlewares/createCookie.js
@@ -8,12 +8,13 @@ exports.createCookie = (request, response) => {
       specialization_id: res.rows[0].specalization_id,
       firstname: res.rows[0].firstname,
       lastname: res.rows[0].lastname,
+      photo_url: res.rows[0].photo_url,
     }))
     .then((res) => {
       const token = jwt.sign(res, process.env.SECRET);
       response.cookie('token', token, { httpOnly: true });
     }).catch(() => {
-      response.status(500).send(JSON.stringify({ ErrMsg: 'Internal server error' }));
+      response.status(500).send({ ErrMsg: 'Internal server error' });
     });
   response.redirect('/');
 };
