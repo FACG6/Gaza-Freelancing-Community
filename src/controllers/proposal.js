b8b9725be7d9001ecb8bf7802762be7cd3c74160
Angham116 <angham.1161995@hotmail.com>
2019-03-25 15:29:29 +0200

    render proposal page

--- src/controllers/proposal.js
+++ src/controllers/proposal.js
@@ -1,8 +1,19 @@
+const getProposal = require('./../database/queries/getData');
+
+// console.log('ppppppppppp', getProposal.getProposal);
 
 exports.get = (request, response) => {
-  response.render('proposal', {
-    layout: 'main',
-    css: ['proposal'],
-    title: 'Proposal',
-  });
+  // console.log(4444444444444, request.url.split('/')[2]);
+  const specId = request.url.split('/')[2];
+  getProposal.getProposal(specId).then((result) => {
+    console.log('kkkkkkkkkk', result.rows[0]);
+    const proposal = result.rows[0];
+    response.render('proposal', {
+      proposal,
+      layout: 'main',
+      css: ['proposal'],
+      title: 'Proposal',
+    });
+  })
+    .catch(err => console.log('jjjjjjjjjjjjjjjjjj', err));
 };
