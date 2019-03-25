c5706335ef23619fb0102fa24195327d3269e4ad
Fatma Siam <fatma.o.siam@gmail.com>
2019-03-26 02:03:59 +0300

    testing the Queries

--- test/testingQuery.js
+++ test/testingQuery.js
@@ -1,13 +1,12 @@
 const tape = require('tape');
 const reBuildDB = require('../src/database/config/db_build');
-const { getProposalbyUserId } = require('./../src/database/queries/getData');
+const { getCategories, getSpecalize, checkMobile } = require('./../src/database/queries/getData');
 
-
-tape('test getProposalbyUserId for \'valid propsal\'', (t) => {
+tape('test getCategories ', (t) => {
   reBuildDB()
-    .then(() => getProposalbyUserId(1))
+    .then(() => getCategories())
     .then((res) => {
-      t.equal(res.rows[0].description, 'we need a front-end developer to working at project', ' Must Retrun description');
+      t.deepEqual(res.rows[0].name, 'Developer', ' Must Retrun name of the first category');
       t.end();
     })
     .catch((errr) => {
@@ -16,9 +15,22 @@ tape('test getProposalbyUserId for \'valid propsal\'', (t) => {
     });
 });
 
-tape('test getProposalbyUserId for \'invalid proposal\'', (t) => {
+tape('test getSpecalize for \' valid value\'', (t) => {
   reBuildDB()
-    .then(() => getProposalbyUserId(3))
+    .then(() => getSpecalize(1))
+    .then((res) => {
+      t.equal(res.rows[0].name, 'web develpoer', 'must return name of the firsr specalization');
+      t.end();
+    })
+    .catch((errr) => {
+      t.error(errr);
+      t.end();
+    });
+});
+
+tape('test getSpecalize for \' invalid value\'', (t) => {
+  reBuildDB()
+    .then(() => getSpecalize(20))
     .then((res) => {
       t.equal(res.rows[0], undefined, 'must return undefined');
       t.end();
@@ -28,6 +40,34 @@ tape('test getProposalbyUserId for \'invalid proposal\'', (t) => {
       t.end();
     });
 });
+
+
+tape('test checkMobile for \' valid mobile\'', (t) => {
+  reBuildDB()
+    .then(() => checkMobile('0599999999'))
+    .then((res) => {
+      t.equal(res.rows[0].mobile_number, '0599999999', 'must return row');
+      t.end();
+    })
+    .catch((errr) => {
+      t.error(errr);
+      t.end();
+    });
+});
+
+tape('test checkMobile for \' invalid mobile\'', (t) => {
+  reBuildDB()
+    .then(() => checkMobile('0595599999'))
+    .then((res) => {
+      t.equal(res.rows[0], undefined, 'must return undefined');
+      t.end();
+    })
+    .catch((errr) => {
+      t.error(errr);
+      t.end();
+    });
+});
+
 tape.onFinish(() => {
   process.exit(0);
 });
