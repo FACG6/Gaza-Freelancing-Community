7f281fc1c6b9f8800e41a5c5e14781ff178a3a77
Fatma Siam <fatma.o.siam@gmail.com>
2019-03-26 18:38:32 +0300

    Edit signup dom

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,22 +1,48 @@
 const firstStepDiv = getElement('user-info');
 const secondStepDiv = getElement('contacts-info');
 const thirdStepDiv = getElement('confirm-info');
-const confirmedPassword = getElement('confirm-pass','val');
-const fields = document.getElementById("category-fields");
-const specializations = document.getElementById("specialization-fields");
+const confirmedPassword = getElement('confirm-pass', 'val');
+const fieldsdroplst = document.getElementById("category-fields");
+const specializationsdroplst = document.getElementById("specialization-fields");
 
 const userInfo = {};
 secondStepDiv.style.display = 'none';
 thirdStepDiv.style.display = 'none';
 
+
+function OnSelectionChange() {
+  const selectedGategory = fieldsdroplst.options[fieldsdroplst.selectedIndex];
+  specializationsdroplst.textContent = '';
+  const categoryId = selectedGategory.value;
+  fetch('/specialize', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        categoryId
+      }),
+    }).then(res => res.json())
+    .then((res ) => {
+      const speclize = res.speclize.rows;
+      for (let i = 0; i < speclize.length; i++) {
+        const option = document.createElement('option');
+        option.value = speclize[i].id;
+        option.text = speclize[i].name;
+        specializationsdroplst.appendChild(option);
+      }
+    });
+}
+
+
 getElement('user-info-next-btn').addEventListener('click', (e) => {
   e.preventDefault();
-    userInfo.firstSection = {
-      firstName: getElement('firstname', 'val'),
-      lastName: getElement('lastname', 'val'),
-      mobile: getElement( 'mobile', 'val'),
-      email: getElement('email', 'val')
-    }
+  userInfo.firstSection = {
+    firstname: getElement('firstname', 'val'),
+    lastname: getElement('lastname', 'val'),
+    mobile_number: getElement('mobile', 'val'),
+    email: getElement('email', 'val')
+  }
   validate(userInfo.firstSection, validationRegex.firstStepValidationRegex, (trueOrFalse) => {
     if (trueOrFalse) {
       ToggleDisplay(firstStepDiv, secondStepDiv);
@@ -26,10 +52,10 @@ getElement('user-info-next-btn').addEventListener('click', (e) => {
 getElement('to-confirm-info').addEventListener('click', (e) => {
   e.preventDefault();
   userInfo.secondSection = {
-    field: fields.options[fields.selectedIndex].value,
-    specialization:specializations.options[specializations.selectedIndex].value,
-    freeLanceURL: getElement('user-url','val'),
-    photoURL: getElement('user-photo','val'),
+    // field: fieldsdroplst.options[fieldsdroplst.selectedIndex].value,
+    specalization_id: specializationsdroplst.options[specializationsdroplst.selectedIndex].value,
+    freelancer_url: getElement('user-url', 'val'),
+    photo_url: getElement('user-photo', 'val'),
   };
   validate(userInfo.secondSection, validationRegex.secondStepValidationRegex, (trueOrFalse) => {
     if (trueOrFalse) {
@@ -44,20 +70,24 @@ getElement('to-user-info').addEventListener('click', (e) => {
 getElement('signup-btn').addEventListener('click', (e) => {
   e.preventDefault();
   userInfo.thirdSection = {
-    password: getElement('password','val'),
+    password: getElement('password', 'val'),
   };
-  if (userInfo.thirdSection['password'] === getElement('confirm-pass','val')) {
+  console.log(userInfo);
+  if (userInfo.thirdSection['password'] === getElement('confirm-pass', 'val')) {
     validate(userInfo.thirdSection, validationRegex.thirdStepValidationRegex, (trueOrFalse) => {
       if (trueOrFalse) {
         fetch('/signup', {
-          method: 'POST',
-          credentials: 'same-origin',
-          body: JSON.stringify(userInfo),
-          headers: { 'Content-Type': 'application/json' },
-        })
+            method: 'POST',
+            credentials: 'same-origin',
+            body: JSON.stringify(userInfo),
+            headers: {
+              'Content-Type': 'application/json'
+            },
+          })
           .then(res => res.json())
           .then((res) => {
             if (res.Error) {
+              console.log(res.Error);
               Swal.fire({
                 title: '<i>Oops</i>',
                 html: `${res.Error}`,
@@ -76,7 +106,7 @@ getElement('signup-btn').addEventListener('click', (e) => {
           });
       }
     });
-  }else{
+  } else {
     Swal.fire({
       title: '<i>Oops</i>',
       html: 'Passwords don\'t match',
