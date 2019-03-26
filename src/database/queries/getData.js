bb629ad1e12c0bf34e5301235c0543e51db49450
Angham116 <angham.1161995@hotmail.com>
2019-03-26 15:14:32 +0200

    eslint

--- src/database/queries/getData.js
+++ src/database/queries/getData.js
@@ -19,13 +19,13 @@ const getCategories = () => {
 };
 const getSpecalize = (categoryId) => {
   const sql = {
-    text: 'select id , name  from specialization where field_id = $1',
+    text: 'select id, name from specialization where field_id = $1',
     values: [categoryId],
   };
   return connect.query(sql);
 };
 const getProposals = (specId) => {
-  const sql = 'select proposal.title,  proposal.description, proposal.id, users.firstname,users.lastname,users.photo_url  from proposal inner join users on proposal.user_id = users.id where proposal.specalization_id = $1';
+  const sql = 'select proposal.title, proposal.description, proposal.id, users.firstname, users.lastname, users.photo_url from proposal inner join users on proposal.user_id = users.id where proposal.specalization_id = $1';
   const values = [specId];
   return connect.query(sql, values);
 };
