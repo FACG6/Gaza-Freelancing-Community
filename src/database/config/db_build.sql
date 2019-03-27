fbca386df4975d46071cf28511cb7191d2099db7
Fatma Siam <fatma.o.siam@gmail.com>
2019-03-28 00:29:48 +0300

    solve conflict

--- src/database/config/db_build.sql
+++ src/database/config/db_build.sql
@@ -46,11 +46,7 @@ INSERT INTO field (name)
 values ('Developer');
 
 INSERT INTO specialization (name,field_id) 
-<<<<<<< HEAD
-values ('Back-End develpoer',1),('Front-End',1);
-=======
 values ('Front develpoer',1), ('Back-end',1);
->>>>>>> 28ce6754ef0b8b639f37d5d85b4e8ce58d37a4bb
 
 INSERT INTO users (firstname,lastname,email,freelancer_url,photo_url,mobile_number,specalization_id,password)
 values('fatma','siam','f.siam@gmail.com','https://mm.mm.mmmmm','https://mm.mm.mmmmm','0599999999',1,'$2a$10$5.WmgQhO5eYGiB48vkZJvum.J0HEAH3lToyGD8sdIUEjt8xRK2QOW');
