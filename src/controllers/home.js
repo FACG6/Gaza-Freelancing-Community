c528595dd493c82d6dcee48ae463a7bec21fd526
Anies <a2nies2012@gmail.com>
2019-03-27 23:06:35 +0200

    HomeFront

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -3,8 +3,8 @@ const getData = require('./../database/queries/getData');
 exports.get = (req, res) => {
   getData.getProposals(1).then((result) => {
     res.render('home', {
-        proposals: result.rows,
-        js: ['home'],
+      proposals: result.rows,
+      js: ['home'],
       layout: 'main',
       css: ['home'],
       title: 'home',
