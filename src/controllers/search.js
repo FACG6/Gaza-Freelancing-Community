9eec9daca8a1c5be7b729778739f7768a7620146
Fatma Siam <fatma.o.siam@gmail.com>
2019-03-27 14:46:35 +0300

    search route

---
+++ src/controllers/search.js
@@ -0,0 +1,20 @@
+const { getPropsalsbyValue } = require('../database/queries/getData');
+
+exports.search = (req, res) => {
+  const { inputvalue } = { ...req.body };
+  const { specalization_id: specId } = { ...req.auth };
+  getPropsalsbyValue(specId, inputvalue)
+    .then(({ rows: proposal }) => {
+      if (proposal[0]) {
+        res.render('/', {
+          title: 'Home',
+          js: ['search'],
+          css: ['home'],
+          proposal,
+        });
+      } else {
+        res.status(400).send({ Error: 'No result' });
+      }
+    })
+    .catch(() => res.status(400).send({ Error: 'Bad Request' }));
+};
