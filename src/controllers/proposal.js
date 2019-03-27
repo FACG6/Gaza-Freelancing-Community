ecadf5109f49b45343e286aa3d81fca843c50352
Angham116 <angham.1161995@hotmail.com>
2019-03-27 13:57:09 +0200

    renamed variables

--- src/controllers/proposal.js
+++ src/controllers/proposal.js
@@ -1,31 +1,31 @@
 const { getProposal, getRequirement } = require('./../database/queries/getData');
 
-exports.get = (request, response, next) => {
+exports.get = (request, response) => {
   const propId = request.url.split('/')[2];
   getProposal(propId)
     .then((result) => {
-      const requiermentId = result.rows[0].prop_id;
-      getRequirement(requiermentId)
-        .then((res) => {
-          const allProposal = {
-            allRrequiermen: res.rows,
-            proposal: result.rows[0],
-          };
-          return allProposal;
-        })
-        .then((proposalInfo) => {
-          response.render('proposal', {
-            requirment: proposalInfo.allRrequiermen,
-            prop: proposalInfo.proposal,
-            layout: 'main',
-            css: ['proposal'],
-            title: 'Proposal',
+      const proposalId = result.rows[0].prop_id;
+      if (proposalId) {
+        getRequirement(proposalId)
+          .then((res) => {
+            const allProposal = {
+              requirments: res.rows,
+              proposal: result.rows[0],
+            };
+            return allProposal;
+          })
+          .then((propDesc) => {
+            response.render('proposal', {
+              requirment: propDesc.requirments,
+              prop: propDesc.proposal,
+              layout: 'main',
+              css: ['proposal'],
+              title: 'Proposal',
+            });
           });
-        })
-        .catch(() => response.status(400).send({ Error: 'Bad Request' }));
+      }
     })
     .catch(() => {
       response.status(400).send({ Error: 'Bad Request' });
-      next();
     });
 };
