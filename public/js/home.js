86ffa3a515f70e146327c488e1657db319b38681
Angham116 <angham.1161995@hotmail.com>
2019-03-28 12:24:42 +0200

    create post

--- public/js/home.js
+++ public/js/home.js
@@ -1,57 +1,37 @@
-const requirements = document.getElementById('requirements');
-const textArea = document.getElementById('text-area');
-const skills = document.getElementById('skills');
-const addMore = document.getElementById('add-more');
-const deletBtn = document.getElementById('delete');
-const post = document.getElementById('post');
-const i = 0;
-let j = 0;
+const createPost = document.getElementById('createPost');
+const specializationsdroplst = document.getElementById('specialize');
+const moreRequirment = document.getElementById('addMoreRequirment');
+const proposalForm = document.getElementById('form-container');
 
-function append() {
-  requirements.style.display = 'block';
-}
-addMore.addEventListener('click', () => {
-  j++;
-  const newSkill = document.createElement('input');
-  newSkill.setAttribute('type', 'text');
-  newSkill.setAttribute('placeholder', 'Enter a required skill.');
-  newSkill.setAttribute('id', j);
-  skills.appendChild(newSkill);
+moreRequirment.addEventListener('click', (e) => {
+  e.preventDefault();
+  const newRequirment = document.createElement('input');
+  newRequirment.setAttribute('id', 'requirment');
+  proposalForm.appendChild(newRequirment);
 });
-deletBtn.addEventListener('click', () => {
-  if (j >= 1) {
-    j--;
-    skills.removeChild(skills.lastChild);
-  }
-});
-function remove() {
-  if (textArea.value.length === 0) {
-    requirements.style.display = 'none';
-  }
-}
 
-post.addEventListener('click', () => {
-  const skillsArray = [];
-  for (let a = 0; a < j; a++) {
-    skillsArray.push(document.getElementById(a));
-  }
-  const propsl = {
-    description: textArea.value,
-    specialization: document.getElementById('specialization').value,
-    skills: skillsArray,
-  };
-  if (propsl.description.split(' ').join('').length >= 30) {
-    fetch('/create-post', {
-      method: 'POST',
-      credentials: 'same-origin',
-      body: JSON.stringify(propsl),
-      headers: { 'content-type': 'application/json' },
-    });
-  }else{
-    Swal.fire({
-      title: '<i>Sorry..</i>',
-      html: `Enter a description of minimum 30 characters`,
-      confirmButtonText: '<u>ok</u>',
-    });
-  }
+const requirment = document.getElementById('requirment');
+
+createPost.addEventListener('click', (e) => {
+  e.preventDefault();
+  const post = collectData(['postTitle', 'postDescription']);
+  const specId = specializationsdroplst.options[specializationsdroplst.selectedIndex].value;
+  console.log(post);
+  console.log(specId);
+  // console.log(requirment.value);
+
+  // const proposalInfo = {
+  //   post,
+  //   specId,
+  // };
+  // console.log(proposalInfo);
+
+  // fetch('/create-post', {
+  //   method: 'POST',
+  //   credentials: 'same-origin',
+  //   body: JSON.stringify(proposalInfo),
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // });
 });
