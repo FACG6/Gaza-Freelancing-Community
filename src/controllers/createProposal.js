d190a5724db95a27163551b55ec68433b0607aa2
Fatma Siam <fatma.o.siam@gmail.com>
2019-03-28 15:32:58 +0300

    craete Post backend

--- src/controllers/createProposal.js
+++ src/controllers/createProposal.js
@@ -1,17 +1,18 @@
 const { addPost, addRequirment } = require('../database/queries/addData');
 
 exports.createPost = (req, res) => {
-  const prpoInfo = { ...req.body };
-  addPost(prpoInfo.title, prpoInfo.decsription, 1, prpoInfo.specaliza_id)
+  const userid = req.auth.id;
+  const proposal = { ...req.body };
+  const prpoInfo = proposal.post;
+  addPost(prpoInfo.postTitle, prpoInfo.postDescription, userid, prpoInfo.specId)
     .then(({ rows: result }) => {
       const { id } = result[0];
-      const { requirments } = prpoInfo;
-      requirments.forEach((requirment) => {
-        addRequirment(requirment, id).then(() => {
-          res.status(201).send({ success: result.title });
-          res.redirect('/');
-        }).catch(() => res.status(400).send({ Error: 'Error' }));
-      });
+      const { allRequierments } = proposal;
+      return allRequierments.forEach(requirment => addRequirment(requirment, id));
+    })
+    .then(() => {
+      res.status(201).send({ success: 'Success Add Post ' });
+      res.redirect('/');
     })
     .catch(() => res.status(400).send({ Error: 'Error' }));
 };
