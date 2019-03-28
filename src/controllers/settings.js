0f36f1e421d6382bf3d97cb80d097caff7565c66
Angham116 <angham.1161995@hotmail.com>
2019-03-28 09:50:01 +0200

    edit settings

--- src/controllers/settings.js
+++ src/controllers/settings.js
@@ -24,8 +24,17 @@ exports.put = (request, response) => {
     const {
       firstname, lastname, birthday, mobile, userPhoto, email, userUrl,
     } = { ...request.body };
+    // console.log(request.auth.id, firstname, lastname, birthday, mobile, userPhoto, email, userUrl);
     updateUser(request.auth.id, firstname, lastname, birthday, mobile, userPhoto, email, userUrl)
-      .then(result => console.log(result, 111111111))
-      .catch(() => response.status(400).send({ Error: 'Bad Request' }));
+      .then((result) => {
+        console.log(result, 111111111);
+        console.log(2222222222222);
+      })
+      .catch((err) =>
+      {
+        console.log(err);
+
+        response.status(400).send({ Error: 'Bad Request' });
+      });
   }
 };
